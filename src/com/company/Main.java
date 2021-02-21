d5833ea5d9afbdda0cf8786696b4e1c564db352e
Maksim Kalabukhov <bfmaximmm1992@gmail.com>
2021-02-21 13:39:43 +0300

    задание 11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,17 +26,17 @@ public class Main {
         Orange orange = new Orange();
         Box<Apple> boxApple = new Box<>(apple, 4);
         Box<Orange> boxOrange = new Box<>(orange, 4);
-        Box<Apple> appleBox = new Box<>(apple,0);
-        appleBox.addCoreFruit(apple,1);
+        Box<Apple> boxApple2 = new Box<>(apple,0);
+        boxApple2.addCoreFruit(apple,1);
         System.out.println(boxApple.getWeight() + " вес коробки с яблоками 1, " + boxApple.getCoreFruit() + " яблока");
         System.out.println(boxOrange.getWeight() + " вес коробки с апельсинами 1, " + boxOrange.getCoreFruit() + " апельсина");
         System.out.println(boxApple.compare(boxOrange) + " сравнение коробки, яблоки и апельсины 1 и 1");
-        System.out.println(appleBox.getWeight() + " вес коробки с яблоками 2, " + appleBox.getCoreFruit() + " яблока");
-        boxApple.inBoxInBox(appleBox);
+        System.out.println(boxApple2.getWeight() + " вес коробки с яблоками 2, " + boxApple2.getCoreFruit() + " яблока");
+        boxApple.inBoxInBox(boxApple2);
         boxApple.addCoreFruit(apple,1);
         System.out.println("Переспали яблоки из 2 коробки в 1 и добавили 1 яблоко");
         System.out.println(boxApple.getWeight() + " вес коробки яблоки 1 стал после пересыпания, " + boxApple.getCoreFruit() + " яблока");
-        System.out.println(appleBox.getWeight() + " вес коробки яблоки 2, " + appleBox.getCoreFruit() + " яблока");
+        System.out.println(boxApple2.getWeight() + " вес коробки яблоки 2, " + boxApple2.getCoreFruit() + " яблока");
         System.out.println(boxApple.compare(boxOrange) + " сравнение яблоки и апельсины 1 и 1");
 
     }
